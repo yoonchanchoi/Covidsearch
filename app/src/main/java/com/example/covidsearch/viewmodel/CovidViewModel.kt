d286b997e0e23263a871ae205932fb8065a4edae
ChoiYoonChan <choyc9239@gmail.com>
2022-06-11 15:42:35 +0900

    observable search 구현 리펙토링

--- app/src/main/java/com/example/covidsearch/viewmodel/CovidViewModel.kt
+++ app/src/main/java/com/example/covidsearch/viewmodel/CovidViewModel.kt
@@ -19,7 +19,7 @@ class CovidViewModel(private val repo: CovidRepository) : ViewModel() {
     private val _liveStateVo: MutableLiveData<StateVO> = MutableLiveData()
     private val _liveToast: MutableLiveData<String> = MutableLiveData()
     private val disposable = CompositeDisposable()
-    private val _liveSearch: MutableLiveData<String> = MutableLiveData()
+    private val _liveSearch: MutableLiveData<ArrayList<CovidVO?>> = MutableLiveData()
 
 
     val liveCovidVo: LiveData<ArrayList<CovidVO?>>
@@ -67,26 +67,65 @@ class CovidViewModel(private val repo: CovidRepository) : ViewModel() {
             }).addTo(disposable)
     }
 
-    fun search(key: String){
-        val filterString = charSequence.toString()
-        val results = FilterResults()
+    fun search(key: String, text: String){
+        repo.getCovidInfo(key)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                //여기에서 라이브데이터에 받은 데이터를 넣어줘야될것으로 보이는 맞나?
+                //맞으면 어떻게 넣어줘야됨?
+                if (text.equals()){
 
-            //검색이 필요없을 경우를 위해 원본 배열을 복제
-        val filteredList: ArrayList<CovidVo> = ArrayList<CovidVo>()
-            //공백제외 아무런 값이 없을 경우 -> 원본 배열
-        if (filterString.trim { it <= ' ' }.isEmpty()) {
-            results.values = CovidVO
-            results.count = CovidVO.size
-
-            return results
-                //공백제외 2글자 이하인 경우 -> 이름으로만 검색
-        } else if (filterString.trim { it <= ' ' }.length <= 2) {
-            for (person in persons) {
-                if (person.name.contains(filterString)) {
-                    filteredList.add(person)
                 }
-            }
-        }
+
+                val regionList = arrayListOf(
+                    it?.korea,
+                    it?.seoul,
+                    it?.busan,
+                    it?.incheon,
+                    it?.gwangju,
+                    it?.jeonbuk,
+                    it?.chungbuk,
+                    it?.jeonnam,
+                    it?.gyeongbuk,
+                    it?.daegu,
+                    it?.ulsan,
+                    it?.daejeon,
+                    it?.sejong,
+                    it?.chungnam,
+                    it?.gyeonggi,
+                    it?.gyeongnam,
+                    it?.gangwon,
+                    it?.jeju,
+                    it?.quarantine
+                )
+                Log.d("성공성공!", regionList.toString())
+                _liveCovidVo.postValue(regionList)
+
+            }, {
+                Log.d("실패실패..", "${it.localizedMessage.toString()}")
+                Log.d("실패실패..", "${it.message.toString()}")
+                _liveToast.postValue(it.localizedMessage)
+            }).addTo(disposable)
+//        val filterString = charSequence.toString()
+//        val results = FilterResults()
+//
+//            //검색이 필요없을 경우를 위해 원본 배열을 복제
+//        val filteredList: ArrayList<CovidVo> = ArrayList<CovidVo>()
+//            //공백제외 아무런 값이 없을 경우 -> 원본 배열
+//        if (filterString.trim { it <= ' ' }.isEmpty()) {
+//            results.values = CovidVO
+//            results.count = CovidVO.size
+//
+//            return results
+//                //공백제외 2글자 이하인 경우 -> 이름으로만 검색
+//        } else if (filterString.trim { it <= ' ' }.length <= 2) {
+//            for (person in persons) {
+//                if (person.name.contains(filterString)) {
+//                    filteredList.add(person)
+//                }
+//            }
+//        }
     }
 
 //    inner class ItemFilter : Filter() {
